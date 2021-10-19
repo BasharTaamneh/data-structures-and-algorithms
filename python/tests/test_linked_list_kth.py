5170cf7a44061b6df72e63eeb6c73817ab47b0d6
bashar adel taamneh <bashartaamneh55@gmail.com>
2021-10-19 17:53:15 +0300

    feat: test_node_has_int_data

--- python/tests/test_linked_list_kth.py
+++ python/tests/test_linked_list_kth.py
@@ -1,1 +1,18 @@
-from linked_list.linked_list_kth.linked_list_kth import Kth
+from linked_list.linked_list_kth.linked_list_kth import LinkedList, Node
+
+
+def test_import():
+    assert LinkedList()
+    assert Node(1)
+
+
+def test_node_has_int_data():
+    # Arrange any data that you need to run your test
+    expected = 1
+
+    # Act on the subject of the test to produce some actual output
+    node = Node(1)
+    actual = node.data
+
+    # Assert
+    assert actual == expected
