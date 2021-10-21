7157d24fcaa80de438414836cb44a3e578313909
bashar adel taamneh <bashartaamneh55@gmail.com>
2021-10-21 17:27:56 +0300

    queue_&_stack_challenge_done

--- python/tests/test_linked_list_zip.py
+++ python/tests/test_linked_list_zip.py
@@ -1,2 +1,19 @@
 
-from  linked_list.linked_list_zip import Node, LinkedList
+# from  linked_list.linked_list_zip.linked_list_zip import Node, LinkedList
+
+
+# def test_zip():
+#     ll1 = LinkedList()
+#     ll2 = LinkedList()
+#     ll1.append(1)
+#     ll1.append(3)
+#     ll1.append(2)
+#     ll2.append(5)
+#     ll2.append(9)
+#     ll2.append(4)
+#     ll2.append(40)
+#     ll1.merge(ll1, ll2)
+#     act = (_ for _ in ll1.printList())
+#     actual = act
+#     expected = 1, 5, 3, 9, 2, 4
+#     assert actual == expected
