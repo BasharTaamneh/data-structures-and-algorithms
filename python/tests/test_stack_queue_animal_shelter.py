1d5bbb01907143467507046ffef6ddd857c13343
bashar adel taamneh <bashartaamneh55@gmail.com>
2021-10-26 00:35:30 +0300

    feat: test_AnimalShelter_enqueue_anipt_ether_dog_or_cat

--- python/tests/test_stack_queue_animal_shelter.py
+++ python/tests/test_stack_queue_animal_shelter.py
@@ -1,5 +1,5 @@
 from linked_list.stack_queue_animal_shelter.stack_queue_animal_shelter import Cat, Dog, Anypit, AnimalShelter
-
+import pytest
 
 
 def test_importin():
@@ -9,3 +9,25 @@ def test_importin():
     assert AnimalShelter()
 
 
+def test_AnimalShelter_is_empty():
+    shilter = AnimalShelter()
+    with pytest.raises(Exception):
+        actual = shilter.dequeue("")
+        expected = "AnimalShelter is empty."
+        assert actual == expected
+
+
+def test_AnimalShelter_enqueue_anipt_ether_dog_or_cat():
+    shilter = AnimalShelter()
+    anypit = Anypit("paired")
+    actual = shilter.enqueue(anypit)
+    expected = False
+    assert actual == expected
+
+
+def test_AnimalShelter_enqueue_cat():
+    shilter = AnimalShelter()
+    anypit = Anypit("paired")
+    actual = shilter.enqueue(anypit)
+    expected = False
+    assert actual == expected
