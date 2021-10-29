45be77502b77f9681eb8093ed20c5ee7c74f825e
bashar adel taamneh <bashartaamneh55@gmail.com>
2021-10-30 00:22:38 +0200

    feat: test_add_a_left_child_and_right_child_to_a_single_root_node

--- python/tests/test_trees.py
+++ python/tests/test_trees.py
@@ -9,8 +9,8 @@ def test_instantiate_an_empty_tree():
     assert Node("")
 
 
-# def test_instantiate_tree_with_a_single_root_node():
-#     root = Node(0)
-#     actual = root.data
-#     expected = 0
-#     assert actual == expected
+def test_instantiate_tree_with_a_single_root_node():
+    root = Node(0)
+    actual = root.data
+    expected = 0
+    assert actual == expected
