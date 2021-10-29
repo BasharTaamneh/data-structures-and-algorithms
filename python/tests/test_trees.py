2d00e5e014cdf39602fb338fccedc470659b43e2
bashar adel taamneh <bashartaamneh55@gmail.com>
2021-10-30 00:52:22 +0200

    feat: test_if_a_tree_contain_a_node_at_least_once

--- python/tests/test_trees.py
+++ python/tests/test_trees.py
@@ -60,17 +60,38 @@ def test_return_a_collection_from_an_inorder_traversal():
     assert actual == expected
 
 
-# def test_return_a_collection_from_a_postorder_traversal():
-#     BT = BinaryTree()
-#     root = Node(27)
-#     root.Add(15)
-#     root.Add(35)
-#     root.Add(10)
-#     root.Add(100)
-#     root.Add(201)
-#     root.Add(310)
-#     root.Add(310)
-#     root.Add(310)
-#     actual = BT.Postorder(root)
-#     expected = [10, 15, 310, 310, 310, 201, 100, 35, 27]
-#     assert actual == expected
+def test_return_a_collection_from_a_postorder_traversal():
+    BT = BinaryTree()
+    root = Node(27)
+    root.Add(15)
+    root.Add(35)
+    root.Add(10)
+    root.Add(100)
+    root.Add(201)
+    root.Add(310)
+    root.Add(310)
+    root.Add(310)
+    actual = BT.Postorder(root)
+    expected = [10, 15, 310, 310, 310, 201, 100, 35, 27]
+    assert actual == expected
+
+
+def test_if_a_tree_contain_a_node_at_least_once():
+    root = Node(27)
+    root.Add(15)
+    root.Add(35)
+    root.Add(10)
+    root.Add(100)
+    root.Add(201)
+    root.Add(310)
+    root.Add(310)
+    root.Add(310)
+    actual_1 =  root.contain(1000)
+    actual_2 = root.contain(10)
+    actual_3 = root.contain(27)
+    expected_1 = False
+    expected_2 = True
+    expected_3 = True
+    assert actual_1 == expected_1
+    assert actual_2 == expected_2
+    assert actual_3 == expected_3
