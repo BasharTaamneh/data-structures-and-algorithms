838ad4202dfb33224546346833c124c119019ee0
bashar adel taamneh <bashartaamneh55@gmail.com>
2021-11-14 21:38:25 +0200

    test: test_insertion_sort_1

--- python/tests/test_Insertion_Sort.py
+++ python/tests/test_Insertion_Sort.py
@@ -4,3 +4,11 @@ def test_import():
     assert InsertionSort([0])
 
 
+def test_insertion_sort_1():
+    # Arrange
+    arr = [8, 4, 23, 42, 16, 15]
+    # actual
+    actual = InsertionSort(arr)
+    # expected
+    expected = [4, 8, 15, 16, 23, 42]
+    assert actual == expected
